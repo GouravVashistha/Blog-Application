206410f326411d272cd401c1cf0b0ded26e4ecbb
GOURAV <gvashistha4@gmail.com>
2026-04-08 01:01:49 +0530

    validation

--- src/main/java/com/blog_app_apis/dtos/UserDTO.java
+++ src/main/java/com/blog_app_apis/dtos/UserDTO.java
@@ -2,7 +2,8 @@ package com.blog_app_apis.dtos;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,15 +12,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonPropertyOrder({ "id", "name", "email", "password", "about" })
+@JsonPropertyOrder({"id", "name", "email", "password", "about"})
 public class UserDTO {
     private Integer id;
-    @NotBlank
+    @NotEmpty
+    @Size(min = 4, message = "User must be min  of 4 Character")
     private String name;
     @Email
-    @NotBlank
+    @NotEmpty
     private String email;
-    @NotBlank
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "Password must be at least 3 digits and max is 10..!!")
     private String password;
+    @NotEmpty
     private String about;
 }
