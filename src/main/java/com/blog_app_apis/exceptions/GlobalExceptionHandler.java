6b27a737459726de13eecb842e9402430408f72c
GOURAV <gvashistha4@gmail.com>
2026-04-08 00:43:04 +0530

    add model mapper

---
+++ src/main/java/com/blog_app_apis/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.blog_app_apis.exceptions;
+
+import com.blog_app_apis.dtos.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Resource Not Found
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Invalid Email
+    @ExceptionHandler(InvalidMailException.class)
+    public ResponseEntity<ApiResponse> handleInvalidMail(InvalidMailException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // Validation Errors (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+
+        return buildResponse(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleGenericException(Exception ex) {
+        return buildResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ApiResponse> buildResponse(String message, HttpStatus status) {
+        ApiResponse response = new ApiResponse(message, false);
+        return new ResponseEntity<>(response, status);
+    }
+}
--- src/main/java/com/blog_app_apis/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/blog_app_apis/serviceImpl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.blog_app_apis.exceptions.InvalidMailException;
 import com.blog_app_apis.exceptions.ResourceNotFoundException;
 import com.blog_app_apis.repository.UserRepo;
 import com.blog_app_apis.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +20,23 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
 
-        @Autowired
-        private UserRepo userRepo;
+    @Autowired
+    private UserRepo userRepo;
 
-        @Override
-        public UserDTO createUser(UserDTO userDTO) throws InvalidMailException {
+    @Autowired
+    private ModelMapper modelMapper;
 
-            User user =  this.dtoToUser(userDTO);
-            try {
-                User savedUser = userRepo.save(user);
-                return userToDto(savedUser);
-            } catch (DataIntegrityViolationException ex) {
-                throw new InvalidMailException("Email already exists");
-            }
+    @Override
+    public UserDTO createUser(UserDTO userDTO) throws InvalidMailException {
+
+        User user = modelMapper.map(userDTO, User.class);
+        try {
+            User savedUser = userRepo.save(user);
+            return modelMapper.map(user, UserDTO.class);
+        } catch (DataIntegrityViolationException ex) {
+            throw new InvalidMailException("Email already exists");
         }
+    }
 
     @Override
     public UserDTO updateUser(UserDTO userDTO, Integer userId) throws InvalidMailException {
@@ -58,51 +62,33 @@ public class UserServiceImpl implements UserService {
             user.setAbout(userDTO.getAbout());
 
         try {
-            return userToDto(userRepo.save(user));
+            User savedUser = userRepo.save(user);
+            return modelMapper.map(savedUser, UserDTO.class);
         } catch (DataIntegrityViolationException ex) {
             throw new InvalidMailException("Email already in use");
         }
     }
 
-        @Override
-        public UserDTO getUserById(Integer userId) {
-            User user = getUserOrThrow(userId);
-            return this.userToDto(user);
-        }
+    @Override
+    public UserDTO getUserById(Integer userId) {
+        User user = getUserOrThrow(userId);
+        return modelMapper.map(user, UserDTO.class);
+    }
 
-        @Override
-        public List<UserDTO> getAllUsers() {
-            List<User>  users = this.userRepo.findAll();
-//            List<UserDto> UserDtos = users.stream().map(user -> this.userToDto(user)).collect(Collectors.toList());
-//            return UserDtos;
-            return users.stream().map(this::userToDto).collect(Collectors.toList());
-        }
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userRepo.findAll()
+                .stream()
+                .map(u -> modelMapper.map(u, UserDTO.class))
+                .toList(); // Java 16+
+    }
 
-        @Override
-        public void deleteUser(Integer userId) {
-            User user = getUserOrThrow(userId);
-            this.userRepo.delete(user);
-        }
+    @Override
+    public void deleteUser(Integer userId) {
+        User user = getUserOrThrow(userId);
+        this.userRepo.delete(user);
+    }
 
-        private User dtoToUser(UserDTO UserDTO){
-            User user = new User();
-            user.setId(UserDTO.getId());
-            user.setEmail(UserDTO.getEmail());
-            user.setName(UserDTO.getName());
-            user.setPassword(UserDTO.getPassword());
-            user.setAbout(UserDTO.getAbout());
-            return user;
-        }
-
-        public UserDTO userToDto(User user){
-            UserDTO userDTO = new UserDTO();
-            userDTO.setId(user.getId());
-            userDTO.setEmail(user.getEmail());
-            userDTO.setName(user.getName());
-            userDTO.setPassword(user.getPassword());
-            userDTO.setAbout(user.getAbout());
-            return userDTO;
-        }
 
     private User getUserOrThrow(Integer userId) {
         return userRepo.findById(userId)
