7fbafd0563bfdd9ac1cac1b0eb4247a4a841bacc
GOURAV <gvashistha4@gmail.com>
2026-04-16 00:16:41 +0530

    post controller

--- src/main/java/com/blog_app_apis/controllers/PostController.java
+++ src/main/java/com/blog_app_apis/controllers/PostController.java
@@ -7,12 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+    //======================================= Create Post==============================================
 
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO,
@@ -22,6 +26,27 @@ public class PostController {
         PostDTO createPost = this.postService.createPost(postDTO, userId, categoryId);
         return new ResponseEntity<PostDTO>(createPost, HttpStatus.CREATED);
     }
+    //===================================== Get Post By Category ======================================
 
+    @GetMapping("/category/{categoryId}/posts")
+    public ResponseEntity<List<PostDTO>> getPostByCategory(@PathVariable Integer categoryId) {
+        List<PostDTO> posts = this.postService.getPostByCategory(categoryId);
+        return new ResponseEntity<List<PostDTO>>(posts, HttpStatus.OK);
+    }
+    //======================================= Get Post By User =======x=================================
+
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostDTO>> getPostByUser(@PathVariable Integer userId) {
+        List<PostDTO> posts = this.postService.getPostByUser(userId);
+        return new ResponseEntity<List<PostDTO>>(posts, HttpStatus.OK);
+    }
+
+    //======================================= Update Post ===========================================
+
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<PostDTO> UpdatePost(@RequestBody PostDTO postDto, @PathVariable Integer postId) {
+        PostDTO updatePost = this.postService.updatePost(postDto, postId);
+        return new ResponseEntity<PostDTO>(updatePost, HttpStatus.OK);
+    }
 
 }
