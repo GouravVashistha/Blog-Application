2681d215fda991e8b5376625ce14c0c93383d846
GOURAV <gvashistha4@gmail.com>
2026-05-05 00:16:56 +0530

    jwt

--- src/main/java/com/blog_app_apis/config/SecurityConfig.java
+++ src/main/java/com/blog_app_apis/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -47,7 +48,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // Disable CSRF protection (REST APIs don't need it)
-//                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 // Authorize all HTTP requests
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest()
